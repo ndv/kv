12fef5aba947b42981900c497f2768297b7bb1c5
dmitry <dmitry.negoda@gmail.com>
2019-11-05 12:07:09 +0400

    Initial

---
+++ main/Database.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"github.com/ndv/kv/bitcurve"
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/errors"
+	"github.com/syndtr/goleveldb/leveldb/filter"
+	"github.com/syndtr/goleveldb/leveldb/opt"
+	"github.com/syndtr/goleveldb/leveldb/util"
+	"sync"
+)
+
+type Database struct {
+	db       *leveldb.DB
+	quitLock sync.Mutex      // Mutex protecting the quit channel access
+}
+
+func NewDatabase(path string) (*Database, error) {
+	db, err := leveldb.OpenFile(path, &opt.Options{
+		OpenFilesCacheCapacity: 256,
+		BlockCacheCapacity:     256 / 2 * opt.MiB,
+		WriteBuffer:            256 / 4 * opt.MiB, // Two of these are used internally
+		Filter:                 filter.NewBloomFilter(10),
+		DisableSeeksCompaction: true,
+	})
+	if _, corrupted := err.(*errors.ErrCorrupted); corrupted {
+		db, err = leveldb.RecoverFile(path, nil)
+	}
+	if err != nil {
+		return nil, err
+	}
+	// Assemble the wrapper with all the registered metrics
+	return &Database{db: db}, nil
+}
+
+func (db *Database) Close() error {
+	db.quitLock.Lock()
+	defer db.quitLock.Unlock()
+	return db.db.Close()
+}
+
+func (db *Database) Put(pubkey *ecdsa.PublicKey, key []byte, value []byte) error {
+	key = append(bitcurve.CompressPoint(pubkey), key...)
+	return db.db.Put(key, value, nil)
+}
+
+type Pair struct {
+	key, value []byte
+}
+
+func (db *Database) GetAll(pubkey *ecdsa.PublicKey) ([]Pair, error) {
+	prefix := bitcurve.CompressPoint(pubkey)
+	iterator := db.db.NewIterator(util.BytesPrefix(prefix), nil)
+	defer iterator.Release()
+	if !iterator.First() {
+		return make([]Pair, 0), nil
+	}
+	result := []Pair {Pair{iterator.Key(), iterator.Value()}}
+	for iterator.Next() {
+		result = append(result, Pair{iterator.Key(), iterator.Value()})
+	}
+	return result, nil
+}
---
+++ main/main.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"bufio"
+	"crypto/ecdsa"
+	"crypto/sha256"
+	"encoding/binary"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/ndv/kv/bitcurve"
+	"io"
+	"log"
+	"math/big"
+	"net/http"
+	"os"
+)
+
+var (
+	db *Database
+)
+
+func main() {
+	portNumber := flag.Int("port", 8546, "Port number")
+	flag.Parse()
+
+	defaultPath := os.Getenv("HOME")
+	if defaultPath == "" {
+		defaultPath = "."
+	}
+	databasePath := flag.String("database", defaultPath + "/.kv/database", "Database path")
+	flag.Parse()
+
+	var err error
+	db, err = NewDatabase(*databasePath)
+	if err != nil {
+		log.Fatal("Cannot open %s: %s", *databasePath, err.Error())
+		return
+	}
+	defer db.Close()
+
+	http.HandleFunc("/put", handlePut)
+	http.HandleFunc("/getAll", handleGetAll)
+
+	// Determine port for HTTP service.
+	// Start HTTP main.
+	log.Printf("Listening on port %d", *portNumber)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *portNumber), nil))
+}
+
+func readUint16(r *bufio.Reader) (uint16, error) {
+	bytes := make([]byte, 2)
+	_, err := io.ReadFull(r, bytes)
+	if err == nil {
+		return binary.LittleEndian.Uint16(bytes), nil
+	}
+	return 0, err
+}
+
+func writeUint16(i uint16) []byte {
+	b := make([]byte, 2)
+	binary.LittleEndian.PutUint16(b, i)
+	return b
+}
+
+type CryptoContext struct {
+	s, r   *big.Int
+	pubkey *ecdsa.PublicKey
+}
+
+type WrongPubkeyError struct {}
+
+func (e *WrongPubkeyError) Error() string {
+	return "Wrong compressed public key"
+}
+
+func readRequestHeader(body *bufio.Reader) (*CryptoContext, error) {
+	rbytes := make([]byte, 32)
+	_, err := io.ReadFull(body, rbytes)
+	if err == nil {
+		sbytes := make([]byte, 32)
+		_, err = io.ReadFull(body, sbytes)
+		if err == nil {
+			pubkeyBytes := make([]byte, 33)
+			pubkey := bitcurve.S256().UncompressPoint(pubkeyBytes)
+			if pubkey != nil {
+				return &CryptoContext{s: new(big.Int).SetBytes(sbytes), r: new(big.Int).SetBytes(rbytes), pubkey: pubkey}, nil
+			} else {
+				err = &WrongPubkeyError{}
+			}
+		}
+	}
+	return nil, err
+}
+
+func httpError(err error, w http.ResponseWriter) bool {
+	if err == nil {
+		return false
+	} else {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(400)
+		fmt.Fprintln(w, err.Error())
+		return true
+	}
+}
+
+func (ctx *CryptoContext) checkSignature(message []byte) bool {
+	hash := sha256.Sum256(message)
+	return ecdsa.Verify(ctx.pubkey, hash[:], ctx.r, ctx.s)
+}
+
+func handlePut (w http.ResponseWriter, req *http.Request) {
+	log.Printf("req=%s", req.URL)
+
+	body := bufio.NewReader(req.Body)
+
+	s := make([]byte, 32)
+	_, err := io.ReadFull(body, s)
+	if httpError(err, w) {
+		return
+	}
+	r := make([]byte, 32)
+	_, err = io.ReadFull(body, r)
+	if httpError(err, w) {
+		return
+	}
+	ctx, err := readRequestHeader(body)
+	if httpError(err, w) {
+		return
+	}
+	ksize, err := readUint16(body)
+	if httpError(err, w) {
+		return
+	}
+	key := make([]byte, ksize)
+	_, err = io.ReadFull(body, key)
+	if httpError(err, w) {
+		return
+	}
+	vsize, err := readUint16(body)
+	if httpError(err, w) {
+		return
+	}
+	value := make([]byte, vsize)
+	_, err = io.ReadFull(body, value)
+	if httpError(err, w) {
+		return
+	}
+
+	message := append(writeUint16(ksize), key...)
+	message = append(message, writeUint16(vsize)...)
+	message = append(message, value...)
+
+	if !ctx.checkSignature(message) {
+		w.WriteHeader(403)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		fmt.Fprintln(w, "Wrong signature")
+	} else {
+		db.Put(ctx.pubkey, key, value)
+		w.WriteHeader(200)
+	}
+}
+
+func handleGetAll (w http.ResponseWriter, req *http.Request) {
+	log.Printf("req=%s", req.URL)
+
+	body := bufio.NewReader(req.Body)
+
+	ctx, err := readRequestHeader(body)
+	if httpError(err, w) {
+		return
+	}
+
+	list, err := db.GetAll(ctx.pubkey)
+	if httpError(err, w) {
+		return
+	}
+
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(list)
+}
